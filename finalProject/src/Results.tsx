b18f3a38e068422ab1d021026e4dbab39ca05a4e
Eugene Kang <kangeuge90@gmail.com>
2022-01-28 14:13:51 -0500

    added hasDishTypes

--- finalProject/src/Results.tsx
+++ finalProject/src/Results.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 interface Result {
-  id: number,
+  id: number;
   title: string;
   image: string;
-  missedIngredientCount: number,
+  missedIngredientCount: number;
   extendedIngredients: any[];
+  dishTypes: any[];
   servings: number;
   instructions: string;
   link: string;
@@ -19,48 +20,55 @@ interface Result {
 
 // extended ingredients interface ?
 
-export function ResultList({searchTerm}:{searchTerm: string}) {
-  const [results, setResults] = useState<Result[]>([])
+export function ResultList({ searchTerm }: { searchTerm: string }) {
+  const [results, setResults] = useState<Result[]>([]);
 
   useEffect(() => {
     const options = {
-
       params: {
         ingredients: searchTerm,
-        number: '5',
+        number: "30",
         ignorePantry: false,
-        ranking: '2',
-        type: 'drink'
+        ranking: "2",
       },
       headers: {
-        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-        'x-rapidapi-key': 'bac5b9dc17msh2d4a352662dd6a7p190c49jsn86be79361b22'
-      }
+        "x-rapidapi-host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "bac5b9dc17msh2d4a352662dd6a7p190c49jsn86be79361b22",
+      },
     };
-    
-    axios.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients', options).then(function (response: any) {
-      console.log(response.data);
-      // sorting logic here
-  
-      const cocktails = response.data as Result[];
-  
 
-      setResults(cocktails.filter( cocktail  => cocktail.missedIngredientCount <= 5 
-        && cocktail.id != 506555 
-        ))
-      // setResults(cocktails)
-  
-    }).catch(function (error: any) {
-      console.error(error);
-    });
-  }, [searchTerm])
-  return(
+    axios
+      .get(
+        "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients",
+        options
+      )
+      .then(function (response: any) {
+        console.log(response.data);
+        // sorting logic here
+
+        const cocktails = response.data as Result[];
+
+        setResults(
+          cocktails.filter(
+            (cocktail) =>
+              cocktail.missedIngredientCount <= 5 && cocktail.id != 506555
+          )
+        );
+        // setResults(cocktails)
+      })
+      .catch(function (error: any) {
+        console.error(error);
+      });
+  }, [searchTerm]);
+  return (
     <div>
       <h2>Results for {searchTerm}</h2>
-    {results.map((result) => {return <ResultItem key={result.id} result={result}></ResultItem>})}
-
+      {results.map((result) => {
+        return <ResultItem key={result.id} result={result}></ResultItem>;
+      })}
     </div>
-  )
+  );
 }
 
 export function ResultItem({ result }: { result: Result }) {
@@ -70,77 +78,161 @@ export function ResultItem({ result }: { result: Result }) {
     setHidden(!hidden);
   }
 
-  const [fullResults, setFullResults] = useState<Result>(result)
+  const [fullResults, setFullResults] = useState<Result>(result);
 
   useEffect(() => {
     const options = {
-
       headers: {
-        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-        'x-rapidapi-key': 'bac5b9dc17msh2d4a352662dd6a7p190c49jsn86be79361b22'
-      }
+        "x-rapidapi-host":
+          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "bac5b9dc17msh2d4a352662dd6a7p190c49jsn86be79361b22",
+      },
     };
-    
 
-    axios.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${result.id}/information`, options).then(function (response: any) {
-      console.log(response.data);
-      // sorting logic here
-  
-      const cocktails = response.data as Result;
+    axios
+      .get(
+        `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${result.id}/information`,
+        options
+      )
+      .then(function (response: any) {
+        console.log(response.data);
+        // sorting logic here
 
-      // setResults(cocktails.filter( cocktail  => cocktail.missedIngredientCount === 0))
-      setFullResults(cocktails)
-  
-    }).catch(function (error: any) {
-      console.error(error);
-    });
-  }, [result])
+        const cocktails = response.data as Result;
 
+        // setResults(cocktails.filter( cocktail  => cocktail.missedIngredientCount === 0))
+        setFullResults(cocktails);
+      })
+      .catch(function (error: any) {
+        console.error(error);
+      });
+  }, [result]);
 
   // toggle(index: number) {
 
   //     result.hidden[index] = !result.hidden[index]
   //     this.setState({ reviews })
   //   }
-  
-const hasAlcohol = fullResults.extendedIngredients?.some((ingredient) => {
-  console.log(ingredient);
-  const allowedIngredients = ['Alcoholic Beverages', 'Alcohol', 'Beverage', 'Beverages', 'Liquor', 'alcoholic beverages', 'alcohol', 'Party', 'party']
-  return allowedIngredients.includes(ingredient.aisle) 
-}  );
 
-console.log(fullResults.extendedIngredients);
+  // jan 27 left off here
+  // Check for dishType as alcohol first before allowedIngredients of alcohol
 
-  if ( hasAlcohol ) {
-  return (
-    <ul className="resultsListContainer">
-      <li>
-        <p>{result.title}</p>
-        <img src={result.image} onClick={toggleHidden}></img>
-        {result.missedIngredientCount === 1 && <p>You are missing 1 ingredient</p>}
-        {result.missedIngredientCount > 1 && <p>You are missing {result.missedIngredientCount} ingredients</p>}
-        {/* want to call API to search ID upon click */}
-        {hidden === true && (
-          <div className="moreDetailsContainer">
-            <ul>{fullResults.extendedIngredients.map((ingredient: any) => {
-              return(
-                <li>{ingredient.originalString}</li>
-              )
-            })}</ul>
-            <p> {fullResults.instructions} </p>
-            <a href={fullResults.sourceUrl}> Original recipe at {fullResults.sourceName} </a>
-            <p> {fullResults.rating} </p>
-            {/* might be image instead */}
-            <p> {fullResults.favorite} </p>
-            {/* might be image instead */}
-          </div>
-        )}
-      </li>
-    </ul>
-  );
-} else {
-  return null;
-}}
+  const hasDishType = fullResults.dishTypes?.some((ingredient) => {
+    console.log(ingredient);
+    const allowedDishTypes = [
+      "Alcoholic Beverages",
+      "Alcohol",
+      "Beverage",
+      "beverage",
+      "Beverages",
+      "beverages",
+      "Liquor",
+      "liquor",
+      "alcoholic beverages",
+      "alcohol",
+      "Party",
+      "party",
+      "drink",
+      "Drink",
+    ];
+    return allowedDishTypes.includes(ingredient.dishTypes);
+  });
+
+  const hasAlcohol = fullResults.extendedIngredients?.some((ingredient) => {
+    console.log(ingredient);
+    const allowedIngredients = [
+      "Alcoholic Beverages",
+      "Alcohol",
+      "Beverage",
+      "beverage",
+      "Beverages",
+      "beverages",
+      "Liquor",
+      "liquor",
+      "alcoholic beverages",
+      "alcohol",
+      "Party",
+      "party",
+      "drink",
+      "Drink",
+    ];
+    return allowedIngredients.includes(ingredient.aisle);
+  });
+
+  console.log(fullResults);
+
+  if (hasAlcohol) {
+    return (
+      <ul className="resultsListContainer">
+        <li>
+          <p>{result.title}</p>
+          <img src={result.image} onClick={toggleHidden}></img>
+          {result.missedIngredientCount === 1 && (
+            <p>You are missing 1 ingredient</p>
+          )}
+          {result.missedIngredientCount > 1 && (
+            <p>You are missing {result.missedIngredientCount} ingredients</p>
+          )}
+          {/* want to call API to search ID upon click */}
+          {hidden === true && (
+            <div className="moreDetailsContainer">
+              <ul>
+                {fullResults.extendedIngredients.map((ingredient: any) => {
+                  return <li>{ingredient.originalString}</li>;
+                })}
+              </ul>
+              <p> {fullResults.instructions} </p>
+              <a href={fullResults.sourceUrl}>
+                {" "}
+                Original recipe at {fullResults.sourceName}{" "}
+              </a>
+              <p> {fullResults.rating} </p>
+              {/* might be image instead */}
+              <p> {fullResults.favorite} </p>
+              {/* might be image instead */}
+            </div>
+          )}
+        </li>
+      </ul>
+    );
+  } else if (hasDishType) {
+    return (
+      <ul className="resultsListContainer">
+        <li>
+          <p>{result.title}</p>
+          <img src={result.image} onClick={toggleHidden}></img>
+          {result.missedIngredientCount === 1 && (
+            <p>You are missing 1 ingredient</p>
+          )}
+          {result.missedIngredientCount > 1 && (
+            <p>You are missing {result.missedIngredientCount} ingredients</p>
+          )}
+          {/* want to call API to search ID upon click */}
+          {hidden === true && (
+            <div className="moreDetailsContainer">
+              <ul>
+                {fullResults.extendedIngredients.map((ingredient: any) => {
+                  return <li>{ingredient.originalString}</li>;
+                })}
+              </ul>
+              <p> {fullResults.instructions} </p>
+              <a href={fullResults.sourceUrl}>
+                {" "}
+                Original recipe at {fullResults.sourceName}{" "}
+              </a>
+              <p> {fullResults.rating} </p>
+              {/* might be image instead */}
+              <p> {fullResults.favorite} </p>
+              {/* might be image instead */}
+            </div>
+          )}
+        </li>
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
 
 // <ul className='ingredientsList'>
 //   {
@@ -151,7 +243,5 @@ console.log(fullResults.extendedIngredients);
 //     onClick={() => {handleDeleteIngredient(i)}}></img>
 //     </li>)
 // }
-
-
 
 export default ResultItem;
